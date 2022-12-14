dd450a2e26b72a41cd3fc94daa5b3bd3603bff1b
MNatiki <natiart07@gmail.com>
2022-12-14 08:21:00 -0800

    egyhfgv

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,16 @@
 
 /**
  * _islower - entry point
+ * @c: The character to be checked
  * Return: 0
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		return(1);
+		return (1);
 	}
 	else
-		return(0);
+		return (0);
 
 }
