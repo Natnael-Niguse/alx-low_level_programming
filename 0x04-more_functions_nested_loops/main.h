1d4ae928c1b52dfa979b2e2e156e5a480fc65508
MNatiki <natiart07@gmail.com>
2022-12-16 02:00:59 -0800

    uifrh

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -16,5 +16,15 @@ void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
 int _isupper(int c);
+void print_most_numbers(void);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif
