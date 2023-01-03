07b16dea7eff061154d9652b0b8ef1c469b80c68
MNatiki <natiart07@gmail.com>
2023-01-03 09:22:11 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,22 +9,17 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int n = 0;
-	int r;
+	unsigned int i, j;
 
-	while (*s)
+	for (j = 0; *(s + j); j++)
 	{
-		for (r = 0; accept[r]; r++)
+		for (i = 0; *(accept + i); i++)
 		{
-			if (*s == accept[r])
-			{
-				n++;
+			if (*(s + j) == *(accept + i))
 				break;
-			}
-			else if (accept[r + 1] == '\0')
-				return (n);
 		}
-		s++;
+		if (*(accept + i) == '\0')
+			break;
 	}
-	return (n);
+	return (j);
 }
