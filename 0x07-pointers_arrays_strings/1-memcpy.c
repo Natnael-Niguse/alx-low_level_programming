3382ae6730199e71854a9d6e53913f0163c27940
MNatiki <natiart07@gmail.com>
2023-01-03 07:51:40 -0800

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - entry point
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: 0
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int h;
+
+	for (h = 0; h < n; h++)
+		dest[i] = src[i];
+	return (dest);
+}
