ec1208d1f971f3b58c60ca739b0850f4f182fce8
MNatiki <natiart07@gmail.com>
2023-01-04 03:57:02 -0800

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - entry point
+ * @x: input
+ * @y: input
+ * Return: -1
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int res = x;
+
+	if (y > 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	res *= _pow_recursion(x, y - 1);
+	return (res);
+}
