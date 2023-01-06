2f8e2ce12fb5be9c92dcfc71c8353e2803ccd2b9
Nati <natiart07@gmail.com>
2023-01-06 10:05:13 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -33,9 +33,9 @@ int _atoi(char *s)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9'
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 
-					break;
+				break;
 			f = 0;
 		}
 		i++;
@@ -58,7 +58,7 @@ int main(int argc, char *argv[])
 
 	if (argc < 3 || argc < 3)
 	{
-		pintf("Error\n");
+		printf("Error\n");
 		return (1);
 	}
 
