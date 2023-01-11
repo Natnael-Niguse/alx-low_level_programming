300e0e30075288695759230e8aff31e777f7e4af
Nati <natiart07@gmail.com>
2023-01-10 22:33:04 -0800

    libmy.a, main.h

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * _puts - Entry point
+ *
+ * @str: para
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+}
