b543e7ed5a83ea2332aa12b4ef03a133a7fa2bb5
Nati <natiart07@gmail.com>
2023-01-17 09:15:03 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,5 @@
-
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - name of the step
  * @name: First member
@@ -8,7 +9,22 @@
 
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
