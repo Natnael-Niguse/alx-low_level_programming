a45c1d7368925df387a10db2829447fed61b942f
MNatiki <natiart07@gmail.com>
2023-03-18 09:22:37 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 7-main.c 2-add_dnodeint.c 3-add_dnodeint_end.c 0-print_dlistint.c 4-free_dlistint.c 7-insert_dnodeint.c -o j

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -5,45 +5,44 @@
 #include "lists.h"
 
 /**
-* insert_dnodeint_at_index - Inserts a new node at a given position in a
-* doubly linked list.
-* @h: Pointer to a pointer to the head node of the list.
-* @idx: The index of the list where the new node should be added.
-* @n: The integer to be stored in the new node.
+* insert_dnodeint_at_index - inserts a new node at a given position
+* @h: pointer to the head of the list
+* @idx: index of the list where the new node should be added
+* @n: value to be stored in the new node
 *
-* Return: The address of the new node, or NULL if it failed.
+* Return: address of the new node, or NULL if it failed
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new_node, *prev_node, *current_node;
+dlistint_t *new_node, *current_node;
 unsigned int i;
 
-if (h == NULL || idx > dlistint_len(*h))
+if (h == NULL)
 return (NULL);
 
 if (idx == 0)
 return (add_dnodeint(h, n));
 
-prev_node = NULL;
 current_node = *h;
-for (i = 0; i < idx; i++)
-{
-prev_node = current_node;
+for (i = 0; current_node != NULL && i < idx - 1; i++)
 current_node = current_node->next;
-}
 
 if (current_node == NULL)
-return (add_dnodeint_end(h, n));
+return (NULL);
 
 new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 return (NULL);
 
 new_node->n = n;
-new_node->prev = prev_node;
-new_node->next = current_node;
-prev_node->next = new_node;
-current_node->prev = new_node;
+new_node->prev = current_node;
+new_node->next = current_node->next;
+
+if (current_node->next != NULL)
+current_node->next->prev = new_node;
+
+current_node->next = new_node;
 
 return (new_node);
 }
+
