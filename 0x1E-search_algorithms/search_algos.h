b9da4e756534256cc97d80644797f27d2dec5b43
MNatiki <natiart07@gmail.com>
2023-10-10 08:43:41 -0700

    search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,24 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <math.h>
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
